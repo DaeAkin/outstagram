93528a17e9eff2906a41ea4d8212f778f1c61944
민동현 <mindonghyeon890@gmail.com>
2020-07-05 14:33:49 +0900

    User 가입이 Nickname 필드추가

--- outstagram-login/src/main/java/com/project/outstagram/domain/member/dto/UserJoinRequest.java
+++ outstagram-login/src/main/java/com/project/outstagram/domain/member/dto/UserJoinRequest.java
@@ -15,9 +15,13 @@ public class UserJoinRequest {
     @NotBlank(message = "비밀번호를 다시 확인해주세요.")
     String password;
 
+    @NotBlank(message = "닉네임을 다시 확인해주세요.")
+    String nickname;
+
     public User toEntity() {
         return User.builder()
                 .email(email)
+                .nickname(nickname)
                 .password(password)
                 .build();
     }
